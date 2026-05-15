67b0c0a0c482e6a29f5351ecb6400cbb84dd05d3
janqoo <amraboalwaffa@stu.okan.edu.tr>
2026-05-15 14:43:16 +0300

    Fix CI: fix withValues and initialValue errors in property widgets

--- lib/features/properties/presentation/widgets/property_card_widget.dart
+++ lib/features/properties/presentation/widgets/property_card_widget.dart
@@ -65,7 +65,7 @@ class PropertyCardWidget extends StatelessWidget {
           ),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withValues(alpha: 0.06),
+              color: Colors.black.withOpacity(0.06),
               blurRadius: 12,
               offset: const Offset(0, 4),
             ),
